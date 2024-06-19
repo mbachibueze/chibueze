30fc52649c3b0293fbfa9017a5be0dcd542dc27b
mbachibueze <mbachibueze27@gmail.com>
2024-06-19 23:49:20 +0100

    commit

--- script.js
+++ script.js
@@ -15,3 +15,28 @@ const navClose = () =>{
   const isOpen = dropDownMenu.classList.contains('open')
   toggleBtnIcon.classList = isOpen ? "fas fa-xmark" : "fas fa-bars"
 }
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const copyIcons = document.querySelectorAll('.fa-copy');
+
+  copyIcons.forEach(icon => {
+    icon.addEventListener('click', function() {
+      const copyId = this.getAttribute('data-copy');
+      const copyText = document.getElementById(copyId).innerText;
+
+      // Create a temporary textarea element to copy the text
+      const textarea = document.createElement('textarea');
+      textarea.value = copyText;
+      document.body.appendChild(textarea);
+      textarea.select();
+      document.execCommand('copy');
+      document.body.removeChild(textarea);
+
+      // Optional: Alert the user that the text has been copied
+      alert(`Copied: ${copyText}`);
+    });
+  });
+});
+
+
